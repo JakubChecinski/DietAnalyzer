e2a5bf36ac79cde0df7bda0f2a27e03c59693912
JakubChecinski <40202132+JakubChecinski@users.noreply.github.com>
2022-01-26 09:59:19 +0100

    Changes for Azure App Service publish
    Change login credentials to username
    Add Azure dependencies

--- DietAnalyzer/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ DietAnalyzer/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -54,6 +54,11 @@ namespace DietAnalyzer.Areas.Identity.Pages.Account
             public string Email { get; set; }
 
             [Required]
+            [DataType(DataType.Text)]
+            [Display(Name = "Username")]
+            public string UserName { get; set; }
+
+            [Required]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [DataType(DataType.Password)]
             [Display(Name = "Password")]
@@ -77,7 +82,7 @@ namespace DietAnalyzer.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new IdentityUser { UserName = Input.Email, Email = Input.Email };
+                var user = new IdentityUser { UserName = Input.UserName, Email = Input.Email };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
